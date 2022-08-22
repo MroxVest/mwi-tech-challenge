6ce9cc4b83cd722aaf5e9ef6fd74d80be9a19140
Megan Vest <mroxvest@icloud.com>
2022-08-21 21:17:17 -0500

    added needed next app files
    added needed next app files with updated necessary code

--- scripts.jsx
+++ scripts.jsx
@@ -1,15 +1,18 @@
-export default function HomePage() {
+const location = ReactDOM.createRoot(document.getElementById('array-row'));
+function HomePage() {
     const array1 = ['Matt Johnson', 'Matt Johnson', 'Bart Paden', 'Ryan Doss', 'Jared Malcolm'];
     const array2 = ['Matt Johnson', 'Bart Paden', 'Bart Paden', 'Jordan Heigle', 'Jordan Heigle', 'Tyler Viles'];
     const resultArray = ['test', 'test'];
 
     return (
-        <>
-            <ul>
-                {resultArray.map((name) => (
+            <>
+                <ul>
+                resultArray.map((name) => (
                     <li key={index}>{name}</li>
-                ))}
-            </ul>
-        </>
+                    ))
+                </ul>
+            </>
     );
 }
+
+location.render(HomePage);
